b2e2364bd77e9e1cadfdd9da309d6ba736c689e7
松井英俊 <partyrockeverydayimshufflin@gmail.com>
2020-01-24 19:44:06 +0900

    before api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,24 +6,43 @@
       <input type="file" @change="upload" id="file" style="display:none;">
     </label>
     <p>Status: {{ message }}</p>
+    <p>Result: {{ result }}</p>
   </div>
 </template>
 
 <script>
 import { Storage } from 'aws-amplify';
+import axios from 'axios'
 export default {
   name: 'home',
   data () {
     return {
-      message: ""
+      message: "",
+      filename: "",
+      result: ""
     }
   },
   methods: {
     upload: async function (e) {
       var files = e.target.files
-      Storage.put(files[0].name, files[0])
-      .then (result => this.message = "uploaded:" + result['key'])
-      .catch(err => this.message = err)
+      var date = new Date().getTime()
+      Storage.put(date + files[0].name, files[0])
+      .then (result => {
+        this.message = "uploaded:" + (this.filename = result['key'])
+        this.analize()
+      }).catch(err => this.message = err)
+    },
+    analize: async function () {
+      const instance = axios.create({
+        baseURL: 'https://kxnxujr7h0.execute-api.ap-northeast-1.amazonaws.com'
+      })
+      instance.post('/default/photoAnalize',{
+        filename: this.filename
+      },).then(response => {
+        this.result = response.data
+      }).catch(error => {
+        this.result = error
+      }).finally()
     }
   }
 }
