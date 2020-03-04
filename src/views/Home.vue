63801a74cc30510508cd0365260db82e096e7223
松井英俊 <partyrockeverydayimshufflin@gmail.com>
2020-03-04 17:23:13 +0900

    add comments on Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -27,38 +27,38 @@
 </template>
 
 <script>
-import { Storage } from 'aws-amplify';
-import axios from 'axios'
+import { Storage } from 'aws-amplify'//画像アップロードに必要なライブラリ
+import axios from 'axios'//API叩くため
 export default {
   name: 'home',
   data () {
     return {
-      message: "",
-      filename: "",
-      result: ""
+      message: "",//ただの表示用
+      filename: "",//アップロードしたファイル名
+      result: ""//Lambdaから帰ってきたRekognitionの判定結果入れる
     }
   },
   methods: {
-    upload: async function (e) {
-      var files = e.target.files
-      var date = new Date().getTime()
-      Storage.put(date + files[0].name, files[0])
+    upload: async function (e) {//ファイルアップロードメソッド
+      var files = e.target.files//ファイルオブジェクトを格納
+      var date = new Date().getTime()//タイムスタンプ取得
+      Storage.put(date + files[0].name, files[0])//タファイル名にイムスタンプくっ付けてファイルアップロード
       .then (result => {
-        this.message = "uploaded:" + (this.filename = result['key'])
-        this.analize()
-      }).catch(err => this.message = err)
+        this.message = "uploaded:" + (this.filename = result['key'])//アップロード結果を表示用変数に格納
+        this.analize()//Rekognition解析のLambdaのAPIを呼ぶ
+      }).catch(err => this.message = err)//エラー処理
     },
-    analize: async function () {
-      const instance = axios.create({
-        baseURL: 'https://kxnxujr7h0.execute-api.ap-northeast-1.amazonaws.com'
+    analize: async function () {//Rekognition解析のLambdaのAPIを呼ぶメソッド
+      const instance = axios.create({//axiosインスタンス生成
+        baseURL: 'https://kxnxujr7h0.execute-api.ap-northeast-1.amazonaws.com'//APIのベースURL設定
       })
-      instance.post('/default/photoAnalize',{
-        filename: this.filename
+      instance.post('/default/photoAnalize',{//APIのパスを設定
+        filename: this.filename//S3にアップロードしたファイル名
       },).then(response => {
-        this.result = response.data.body.Labels
+        this.result = response.data.body.Labels//解析結果をresultに格納
       }).catch(error => {
-        this.result = error
-      }).finally()
+        this.result = error//エラー処理
+      }).finally()//どのみちなんかやりたい場合はここに書く
     }
   }
 }
